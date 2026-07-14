8ae7e1f73aaee149b496510d253eb8af90b34420
Satheeshsk369 <satheeshsk3693@gmail.com>
2026-07-14 09:17:40 +0530

    argslist

--- src/main.zig
+++ src/main.zig
@@ -5,17 +5,22 @@ const config = @import("config.zig");
 pub fn main(init: std.process.Init) !void {
     const gpa = init.gpa;
     const arena = init.arena.allocator();
-    const args = try init.minimal.args.toSlice(arena);
+    var args = init.minimal.args.iterate();
 
     var sync = false;
-    var final_args = args;
-    if (args.len >= 2 and std.mem.eql(u8, args[1], "-S")) {
-        sync = true;
-        var list = std.ArrayList([:0]const u8).empty;
-        try list.append(arena, args[0]);
-        try list.appendSlice(arena, args[2..]);
-        final_args = list.items;
+    var list = std.ArrayList([:0]const u8).empty;
+
+    if (args.next()) |first| try list.append(arena, first);
+    if (args.next()) |second| {
+        if (std.mem.eql(u8, second, "-S")) {
+            sync = true;
+        } else {
+            try list.append(arena, second);
+        }
     }
+
+    while (args.next()) |rest| try list.append(arena, rest);
+    const argslist = list.items;
 
     const config_zon_path = try action.configPath(arena, init.environ_map);
     const config_val = try config.Config.loadOrInit(arena, init.io, config_zon_path);
@@ -27,12 +32,12 @@ pub fn main(init: std.process.Init) !void {
         .environMap = init.environ_map,
         .pathEnv = init.environ_map.get("PATH") orelse "",
         .userConfig = config_val,
-        .args = final_args,
+        .args = argslist,
         .sync = sync,
     };
 
-    const cmd = action.parseCommand(final_args) orelse {
-        std.log.err("Unknown command: {s}.\nUse 'zigup help' for usage details.", .{final_args[1]});
+    const cmd = action.parseCommand(argslist) orelse {
+        std.log.err("Unknown command: {s}.\nUse 'zigup help' for usage details.", .{argslist[1]});
         return;
     };
 
